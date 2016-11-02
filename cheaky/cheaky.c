8b674fb036f4078caeb2ea64e6a38a53ecf03f99
antonijn <a.d.blom@rublosa.nl>
2016-11-02 16:24:15 +0100

    Added show-c and show-w to cheaky
    In order to comply with the GPL.
    
    Also I reintroduced 'quit' as a function.

--- cheaky/cheaky.c
+++ cheaky/cheaky.c
@@ -24,15 +24,60 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static void show_file(const char *path)
+{
+	FILE *f = fopen(path, "rb");
+	if (!f) {
+		perror("failed to find license file!");
+		return;
+	}
+
+	const size_t bsize = 1024;
+	char buf[bsize];
+	size_t ln;
+	while ((ln = fread(buf, 1, bsize, f)) > 0)
+		fwrite(buf, ln, 1, stdout);
+	fflush(stdout);
+	fclose(f);
+}
+static struct chx_value *show_w(CHEAX *c, struct chx_cons *args)
+{
+	show_file(CMAKE_INSTALL_PREFIX "/share/licenses/cheaky/WARRANTY");
+	return NULL;
+}
+static struct chx_value *show_c(CHEAX *c, struct chx_cons *args)
+{
+	show_file(CMAKE_INSTALL_PREFIX "/share/licenses/cheaky/CONDITIONS");
+	return NULL;
+}
+
+static bool quit = false;
+
+static struct chx_value *quit_fun(CHEAX *c, struct chx_cons *args)
+{
+	quit = true;
+	return NULL;
+}
+
 int main(void)
 {
 	CHEAX *c = cheax_init();
+
+	puts("Cheaky, Copyright (C) 2016 Antonie Blom");
+	puts("Cheaky comes with ABSOLUTELY NO WARRANTY; for details type '(show-w)'.");
+	puts("This is free software, and you are welcome to redistribute it");
+	puts("under certain conditions; type '(show-c)' for details.");
+	cheax_defmacro(c, "show-w", show_w);
+	cheax_defmacro(c, "show-c", show_c);
+
+	cheax_defmacro(c, "quit", quit_fun);
+
 	if (cheax_load_prelude(c)) {
 		perror("failed to load prelude");
 		return EXIT_FAILURE;
 	}
 
-	while (true) {
+	while (!quit) {
 		char *input = readline("> ");
 		if (!input)
 			break;
