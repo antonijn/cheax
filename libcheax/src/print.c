7d3f855831ccee14149046e7c41ddf72fa40d047
Antonie Blom <ablom@posteo.net>
2022-02-11 20:51:24 +0100

    Add sostream_expand(), ostream_putchar()
    sostream_expand() to make sure we have enough space before doing
    something silly like try to add a gazillion characters of padding to a
    string.
    
    ostream_putchar() as a more efficient alternative to ostream_printf()
    for single characters.

--- libcheax/src/print.c
+++ libcheax/src/print.c
@@ -25,20 +25,57 @@
 struct ostream {
 	void *info;
 	int (*vprintf)(void *info, const char *frmt, va_list ap);
+	int (*putchar)(void *info, int ch);
 };
 static int
 ostream_printf(struct ostream *stream, const char *frmt, ...)
 {
 	va_list ap;
 	va_start(ap, frmt);
-	stream->vprintf(stream->info, frmt, ap);
+	int res = stream->vprintf(stream->info, frmt, ap);
 	va_end(ap);
+	return res;
+}
+static int
+ostream_putchar(struct ostream *stream, int ch)
+{
+	return stream->putchar(stream->info, ch);
 }
 
 struct sostream {
+	CHEAX *c;
 	char *buf;
 	size_t idx, buf_len;
 };
+
+static int
+sostream_expand(struct sostream *stream, size_t req_buf)
+{
+	if (req_buf <= stream->buf_len)
+		return 0;
+
+	stream->buf_len = req_buf + req_buf / 2;
+	size_t alloc = req_buf;
+	if (alloc <= SIZE_MAX - alloc / 2)
+		alloc += alloc / 2;
+	else
+		alloc = SIZE_MAX;
+
+	char *new_buf = realloc(stream->buf, alloc);
+	if (new_buf == NULL) {
+		free(stream->buf);
+		stream->buf = NULL;
+		stream->buf_len = stream->idx = 0;
+
+		cry(stream->c, "sostream_expand", CHEAX_ENOMEM, "out of memory");
+		return -1;
+	}
+
+	stream->buf = new_buf;
+	stream->buf_len = alloc;
+
+	return 0;
+}
 
 static int
 sostream_vprintf(void *info, const char *frmt, va_list ap)
@@ -52,17 +89,38 @@ sostream_vprintf(void *info, const char *frmt, va_list ap)
 	int msg_len = vsnprintf(stream->buf + stream->idx, rem, frmt, len_ap);
 	va_end(len_ap);
 
+	if (msg_len < 0)
+		goto msg_len_error;
+
 	size_t req_buf = stream->idx + msg_len + 1; /* +1 for null byte */
 	if (req_buf > stream->buf_len) {
-		stream->buf_len = req_buf + req_buf / 2;
-		stream->buf = realloc(stream->buf, stream->buf_len);
+		if (sostream_expand(stream, req_buf) < 0)
+			return -1;
 
 		msg_len = vsnprintf(stream->buf + stream->idx, msg_len + 1, frmt, ap);
+		if (msg_len < 0)
+			goto msg_len_error;
 	}
 
 	stream->idx += msg_len;
 
 	return msg_len;
+
+msg_len_error:
+	cry(stream->c, "sostream_vprintf", CHEAX_EEVAL,
+	    "internal error (vsnprintf returned %d)", msg_len);
+	return -1;
+}
+
+static int
+sostream_putchar(void *info, int ch)
+{
+	struct sostream *stream = info;
+	if (sostream_expand(stream, stream->idx + 1) < 0)
+		return -1;
+
+	stream->buf[stream->idx++] = ch;
+	return ch;
 }
 
 static int
@@ -70,6 +128,13 @@ file_vprintf(void *info, const char *frmt, va_list ap)
 {
 	FILE *f = info;
 	return vfprintf(f, frmt, ap);
+}
+
+static int
+file_putchar(void *info, int ch)
+{
+	FILE *f = info;
+	return fputc(ch, f);
 }
 
 static void ostream_show(CHEAX *c, struct ostream *s, struct chx_value *val);
@@ -91,20 +156,20 @@ ostream_show_basic_type(CHEAX *c, struct ostream *s, struct chx_value *val)
 		ostream_printf(s, "%s", ((struct chx_id *)val)->id);
 		break;
 	case CHEAX_LIST:
-		ostream_printf(s, "(");
+		ostream_putchar(s, '(');
 		for (struct chx_list *list = (struct chx_list *)val; list; list = list->next) {
 			ostream_show(c, s, list->value);
 			if (list->next)
-				ostream_printf(s, " ");
+				ostream_putchar(s, ' ');
 		}
-		ostream_printf(s, ")");
+		ostream_putchar(s, ')');
 		break;
 	case CHEAX_QUOTE:
-		ostream_printf(s, "'");
+		ostream_putchar(s, '\'');
 		ostream_show(c, s, ((struct chx_quote *)val)->value);
 		break;
 	case CHEAX_FUNC:
-		ostream_printf(s, "(");
+		ostream_putchar(s, '(');
 		struct chx_func *func = (struct chx_func *)val;
 		if (func->eval_args)
 			ostream_printf(s, "\\ ");
@@ -116,21 +181,21 @@ ostream_show_basic_type(CHEAX *c, struct ostream *s, struct chx_value *val)
 			ostream_printf(s, "\n  ");
 			ostream_show(c, s, body->value);
 		}
-		ostream_printf(s, ")");
+		ostream_putchar(s, ')');
 		break;
 	case CHEAX_STRING:
-		ostream_printf(s, "\"");
+		ostream_putchar(s, '"');
 		struct chx_string *string = (struct chx_string *)val;
 		for (int i = 0; i < string->len; ++i) {
 			char ch = string->value[i];
 			if (ch == '"')
 				ostream_printf(s, "\\\"");
 			else if (isprint(ch))
-				ostream_printf(s, "%c", ch);
+				ostream_putchar(s, ch);
 			else
 				ostream_printf(s, "\\x%02x", (unsigned)ch & 0xFFu);
 		}
-		ostream_printf(s, "\"");
+		ostream_putchar(s, '"');
 		break;
 	case CHEAX_EXT_FUNC:
 		; struct chx_ext_func *macro = (struct chx_ext_func *)val;
@@ -164,6 +229,7 @@ cheax_print(CHEAX *c, FILE *f, struct chx_value *val)
 	struct ostream ostr;
 	ostr.info = f;
 	ostr.vprintf = file_vprintf;
+	ostr.putchar = file_putchar;
 	ostream_show(c, &ostr, val);
 }
 
@@ -229,7 +295,7 @@ ostream_print_int(struct ostream *ostr, int num, char pad_char, int field_width,
 	}
 
 	for (int j = 0; j < field_width - content_len; ++j)
-		ostream_printf(ostr, "%c", pad_char);
+		ostream_putchar(ostr, pad_char);
 
 	if (num < 0 && pad_char == ' ')
 		ostream_printf(ostr, "-");
@@ -258,6 +324,7 @@ cheax_format(CHEAX *c, const char *fmt, struct chx_list *args)
 	struct ostream ostr;
 	ostr.info = &ss;
 	ostr.vprintf = sostream_vprintf;
+	ostr.putchar = sostream_putchar;
 
 	enum {
 		UNSPECIFIED,
@@ -272,7 +339,7 @@ cheax_format(CHEAX *c, const char *fmt, struct chx_list *args)
 	} sr_flag;                           /* set per specifier */
 
 	char pad_char, misc_spec;            /* ditto */
-	int field_width, precision;       /* --"-- */
+	int field_width, precision;          /* --"-- */
 	bool can_int, can_double, can_other; /* --"-- */
 
 	int cur_arg_idx = 0;
@@ -296,7 +363,7 @@ normal:
 	if (ch == '}')
 		goto single_end_curly;
 
-	ostream_printf(&ostr, "%c", ch);
+	ostream_putchar(&ostr, ch);
 	goto normal;
 
 entered_curly:
@@ -311,7 +378,7 @@ entered_curly:
 	can_int = can_double = can_other = true;
 
 	if (ch == '{') {
-		ostream_printf(&ostr, "%c", ch);
+		ostream_putchar(&ostr, ch);
 		++fmt;
 		goto normal;
 	}
@@ -334,7 +401,7 @@ entered_curly:
 		indexing = AUTO_IDX;
 	}
 
-	goto read_any_format_specs;
+	goto read_any_sr_flag;
 
 read_idx:
 	cur_arg_idx = ch - '0';
@@ -344,7 +411,7 @@ read_idx:
 read_more_idx:
 	ch = *fmt;
 	if (!isdigit(ch))
-		goto read_any_format_specs;
+		goto read_any_sr_flag;
 
 	if (cur_arg_idx > INT_MAX / 10 || (cur_arg_idx * 10) > INT_MAX - (ch - '0')) {
 		cry(c, "format", CHEAX_EVALUE, "index too big");
@@ -355,6 +422,32 @@ read_more_idx:
 	++fmt;
 	goto read_more_idx;
 
+read_any_sr_flag:
+	ch = *fmt;
+	if (ch == '!') {
+		++fmt;
+		goto read_sr_flag;
+	}
+
+	goto read_any_format_specs;
+
+read_sr_flag:
+	ch = *fmt;
+	if (ch == 's') {
+		sr_flag = SR_FLAG_S;
+		++fmt;
+		goto read_any_format_specs;
+	}
+	if (ch == 'r') {
+		sr_flag = SR_FLAG_R;
+		++fmt;
+		goto read_any_format_specs;
+	}
+
+	cry(c, "format", CHEAX_EVALUE, "expected `s' or `r' after `!'");
+	goto error;
+
+
 read_any_format_specs:
 	ch = *fmt;
 
@@ -363,7 +456,7 @@ read_any_format_specs:
 		goto read_format_specs;
 	}
 
-	goto read_any_sr_flag;
+	goto read_closing_curly;
 
 read_format_specs:
 	ch = *fmt;
@@ -440,32 +533,7 @@ read_misc_spec:
 		++fmt;
 	}
 
-	goto read_any_sr_flag;
-
-read_any_sr_flag:
-	ch = *fmt;
-	if (ch == '!') {
-		++fmt;
-		goto read_sr_flag;
-	}
-
 	goto read_closing_curly;
-
-read_sr_flag:
-	ch = *fmt;
-	if (ch == 's') {
-		sr_flag = SR_FLAG_S;
-		++fmt;
-		goto read_closing_curly;
-	}
-	if (ch == 'r') {
-		sr_flag = SR_FLAG_R;
-		++fmt;
-		goto read_closing_curly;
-	}
-
-	cry(c, "format", CHEAX_EVALUE, "expected `s' or `r' after `!'");
-	goto error;
 
 read_closing_curly:
 	ch = *fmt;
@@ -481,6 +549,13 @@ read_closing_curly:
 
 	struct chx_value *arg = arg_array[cur_arg_idx];
 	int ty = cheax_get_type(arg);
+
+	/* this is primarily so we can catch any ENOMEM early if
+	 * field_width is too big, instead of letting the mem use slowly
+	 * creep up as we add more and more padding. */
+	size_t ufield_width = field_width;
+	if (ss.idx > SIZE_MAX - ufield_width || sostream_expand(&ss, ss.idx + ufield_width) < 0)
+		goto error;
 
 	/* Used to gauge if padding is necessary. With numbers, the
 	 * padding should be correct as it is, but there can still be
@@ -501,7 +576,7 @@ read_closing_curly:
 				goto error;
 			}
 
-			ostream_printf(&ostr, "%c", num);
+			ostream_putchar(&ostr, num);
 		} else {
 			ostream_print_int(&ostr, num, pad_char, field_width, misc_spec);
 		}
@@ -539,8 +614,10 @@ read_closing_curly:
 	/* Add padding if necessary.
 	 * TODO this should probably count graphemes rather than bytes */
 	int written = ss.idx - prev_idx;
+	int padding = field_width - written;
+
 	for (int i = 0; i < field_width - written; ++i)
-		ostream_printf(&ostr, "%c", pad_char);
+		ostream_putchar(&ostr, pad_char);
 
 	if (indexing == AUTO_IDX)
 		++cur_arg_idx;
@@ -551,7 +628,7 @@ read_closing_curly:
 single_end_curly:
 	ch = *fmt;
 	if (ch == '}') {
-		ostream_printf(&ostr, "%c", ch);
+		ostream_putchar(&ostr, ch);
 		++fmt;
 		goto normal;
 	}
